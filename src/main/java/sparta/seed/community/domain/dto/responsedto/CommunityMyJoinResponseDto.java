d54c1436c3b16534834d108983b925aef24e927d
Minkyu222341 <qkralsrb2040@naver.com>
2022-09-17 17:12:31 +0900

    [fix] 그룹미션 리턴값 수정

--- src/main/java/sparta/seed/community/domain/dto/responsedto/CommunityMyJoinResponseDto.java
+++ src/main/java/sparta/seed/community/domain/dto/responsedto/CommunityMyJoinResponseDto.java
@@ -1,18 +1,26 @@
 package sparta.seed.community.domain.dto.responsedto;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 public class CommunityMyJoinResponseDto {
 	private Long communityId;
 	private String title;
 	private String img;
+	private double currentPercent;
+	private double successPercent;
+	private String startDate;
+	private String endDate;
+	private String dateStatus;
 
 	@Builder
-	public CommunityMyJoinResponseDto(Long communityId, String title, String img) {
+	public CommunityMyJoinResponseDto(Long communityId, String title, String img, double currentPercent, double successPercent, String startDate, String endDate, String dateStatus) {
 		this.communityId = communityId;
 		this.title = title;
 		this.img = img;
+		this.currentPercent = currentPercent;
+		this.successPercent = successPercent;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.dateStatus = dateStatus;
 	}
 }
