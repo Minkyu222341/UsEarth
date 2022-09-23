9f500dfe5840dd0e60132caaa2c74dbc62b62e30
2m <philomonx1@gmail.com>
2022-09-23 15:38:45 +0900

    [fix] 토큰 undefined로 들어올 때 처리 완료

--- src/main/java/sparta/seed/jwt/TokenProvider.java
+++ src/main/java/sparta/seed/jwt/TokenProvider.java
@@ -145,8 +145,10 @@ public class TokenProvider {
 
   public void validateHttpHeader(HttpServletRequest servletRequest){
     String authorization = servletRequest.getHeader("Authorization");
+    System.out.println("-----------");
+    System.out.println(authorization);
     try {
-      if (authorization != null) {
+      if (!(authorization == null || authorization.equals("undefined"))) {
         String token = authorization.substring(7);
         validateToken(token);
       }
