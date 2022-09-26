4003091a6fea9ab276c03d24c8d0dc6a560b297d
2m <philomonx1@gmail.com>
2022-09-26 19:35:39 +0900

    [fix] 비속어 필터 레디스 적용 / 커뮤니티 작성 적용 / 관리자용 비속어 레디스 주입 api 구현

--- src/main/java/sparta/seed/community/service/SlangService.java
+++ src/main/java/sparta/seed/community/service/SlangService.java
@@ -6,20 +6,28 @@ import sparta.seed.community.domain.Slang;
 import sparta.seed.community.repository.SlangRepository;
 import sparta.seed.exception.CustomException;
 import sparta.seed.exception.ErrorCode;
+import sparta.seed.util.RedisService;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class SlangService {
 	private final SlangRepository slangRepository;
+	private final RedisService redisService;
+
+	public void addSlang(){
+		List<Slang> slangList = slangRepository.findAll();
+		for(Slang slang : slangList){
+			redisService.addSlang(slang.getContent());
+		}
+	}
 
 	public void checkSlang(String word){
-
-		List<Slang> slangList = slangRepository.findAll();
-
-		for (Slang slang : slangList) {
-			if(word.contains(slang.getContent())){
+		Set<String> slangList = redisService.getSlangSet();
+		for (String slang : slangList) {
+			if(word.contains(slang)){
 				throw new CustomException(ErrorCode.DISCOVER_SLANG);
 			}
 		}
