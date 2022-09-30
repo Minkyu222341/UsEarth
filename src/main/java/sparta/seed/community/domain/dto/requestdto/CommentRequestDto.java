4320afc5adb99505489a5cac8f106789513bbaaf
Minkyu222341 <qkralsrb2040@naver.com>
2022-09-30 12:46:41 +0900

    [refactor] 커뮤니티폴더 requestDto @Vaild 추가

--- src/main/java/sparta/seed/community/domain/dto/requestdto/CommentRequestDto.java
+++ src/main/java/sparta/seed/community/domain/dto/requestdto/CommentRequestDto.java
@@ -1,11 +1,14 @@
 package sparta.seed.community.domain.dto.requestdto;
 
 import lombok.Getter;
+import okhttp3.internal.http2.ErrorCode;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Getter
 public class CommentRequestDto {
+	@NotEmpty(message = "내용을 입력해주세요.")
 	private String content;
 	private boolean delete;
 	private String changeNickname;
--- src/main/java/sparta/seed/community/domain/dto/requestdto/CommunityRequestDto.java
+++ src/main/java/sparta/seed/community/domain/dto/requestdto/CommunityRequestDto.java
@@ -3,16 +3,24 @@ package sparta.seed.community.domain.dto.requestdto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @NoArgsConstructor
 @Getter
 public class CommunityRequestDto {
+  @NotEmpty(message = "시작 날짜를 입력해 주세요.")
   private String startDate;
+  @NotEmpty(message = "종료 날짜를 입력해 주세요.")
   private String endDate;
+  @NotEmpty(message = "달성목표를 설정해 주세요.")
   private Integer limitScore;
+  @NotEmpty(message = "참가인원을 설정해 주세요.")
   private Integer limitParticipants;
   private boolean secret;
   private String password;
+  @NotEmpty(message = "제목을 입력해 주세요.")
   private String title;
+  @NotEmpty(message = "내용을 입력해 주세요.")
   private String content;
   private boolean delete;
   private String changeNickname;
